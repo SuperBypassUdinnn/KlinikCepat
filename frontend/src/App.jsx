2de4e51044b5e2a98bd43e4c4c0e78b20f3d45a4
ilham467-spec <ilham467@mhs.usk.ac.id>
2026-06-24 05:05:31 +0700

    feat: protect routes based on user role

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,36 +1,41 @@
-import { Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import ProtectedRoute from './components/ProtectedRoute';
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 // Patient Pages
-import CariKlinik from './pages/Patient/CariKlinik';
-import TriageForm from './pages/Patient/TriageForm';
-import TicketAntrean from './pages/Patient/TicketAntrean';
+import CariKlinik from "./pages/Patient/CariKlinik";
+import TriageForm from "./pages/Patient/TriageForm";
+import TicketAntrean from "./pages/Patient/TicketAntrean";
 
 // Admin Klinik Pages
-import LoginAdmin from './pages/AdminKlinik/LoginAdmin';
-import DashboardAdmin from './pages/AdminKlinik/DashboardAdmin';
+import LoginAdmin from "./pages/AdminKlinik/LoginAdmin";
+import DashboardAdmin from "./pages/AdminKlinik/DashboardAdmin";
 
 // Super Admin Pages
-import ManajemenKlinik from './pages/SuperAdmin/ManajemenKlinik';
-import ManajemenGejala from './pages/SuperAdmin/ManajemenGejala';
+import ManajemenKlinik from "./pages/SuperAdmin/ManajemenKlinik";
+import ManajemenGejala from "./pages/SuperAdmin/ManajemenGejala";
 
 function App() {
   return (
     <>
       <Navbar />
+
       <Routes>
         {/* Patient Routes */}
         <Route path="/" element={<CariKlinik />} />
+
         <Route path="/triage/:klinikId" element={<TriageForm />} />
+
         <Route path="/ticket" element={<TicketAntrean />} />
 
-        {/* Admin Klinik Routes */}
+        {/* Login */}
         <Route path="/admin/login" element={<LoginAdmin />} />
+
+        {/* Admin Klinik Routes */}
         <Route
           path="/admin/dashboard"
           element={
-            <ProtectedRoute>
+            <ProtectedRoute allowedRoles={["klinik_admin"]}>
               <DashboardAdmin />
             </ProtectedRoute>
           }
@@ -40,15 +45,16 @@ function App() {
         <Route
           path="/superadmin/klinik"
           element={
-            <ProtectedRoute>
+            <ProtectedRoute allowedRoles={["superadmin"]}>
               <ManajemenKlinik />
             </ProtectedRoute>
           }
         />
+
         <Route
           path="/superadmin/gejala"
           element={
-            <ProtectedRoute>
+            <ProtectedRoute allowedRoles={["superadmin"]}>
               <ManajemenGejala />
             </ProtectedRoute>
           }
