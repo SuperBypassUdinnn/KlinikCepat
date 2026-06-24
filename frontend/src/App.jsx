8f95ffae85924bc582b283b77ec6a67220d5bace
ilham467-spec <ilham467@mhs.usk.ac.id>
2026-06-24 18:02:46 +0700

    fix: nothing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import ProtectedRoute from "./components/ProtectedRoute";
-import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import ProtectedRoute from "./components/ProtectedRoute";
 
 // Patient Pages
 import CariKlinik from "./pages/Patient/CariKlinik";
 import TriageForm from "./pages/Patient/TriageForm";
 import TicketAntrean from "./pages/Patient/TicketAntrean";
-import CariKlinik from "./pages/Patient/CariKlinik";
-import TriageForm from "./pages/Patient/TriageForm";
-import TicketAntrean from "./pages/Patient/TicketAntrean";
 
 // Admin Klinik Pages
-import LoginAdmin from "./pages/AdminKlinik/LoginAdmin";
-import DashboardAdmin from "./pages/AdminKlinik/DashboardAdmin";
 import LoginAdmin from "./pages/AdminKlinik/LoginAdmin";
 import DashboardAdmin from "./pages/AdminKlinik/DashboardAdmin";
 
@@ -29,28 +21,21 @@ function App() {
     <>
       <Navbar />
 
-
       <Routes>
         {/* Patient Routes */}
         <Route path="/" element={<CariKlinik />} />
 
-
         <Route path="/triage/:klinikId" element={<TriageForm />} />
-
 
         <Route path="/ticket" element={<TicketAntrean />} />
 
         {/* Login */}
-        {/* Login */}
         <Route path="/admin/login" element={<LoginAdmin />} />
-
-        {/* Admin Klinik Routes */}
 
         {/* Admin Klinik Routes */}
         <Route
           path="/admin/dashboard"
           element={
-            <ProtectedRoute allowedRoles={["klinik_admin"]}>
             <ProtectedRoute allowedRoles={["klinik_admin"]}>
               <DashboardAdmin />
             </ProtectedRoute>
@@ -62,23 +47,20 @@ function App() {
           path="/superadmin/klinik"
           element={
             <ProtectedRoute allowedRoles={["superadmin"]}>
-            <ProtectedRoute allowedRoles={["superadmin"]}>
               <ManajemenKlinik />
             </ProtectedRoute>
           }
         />
 
-
         <Route
           path="/superadmin/gejala"
           element={
-            <ProtectedRoute allowedRoles={["superadmin"]}>
             <ProtectedRoute allowedRoles={["superadmin"]}>
               <ManajemenGejala />
             </ProtectedRoute>
           }
         />
-        
+
         <Route
           path="/superadmin/admin-klinik"
           element={
